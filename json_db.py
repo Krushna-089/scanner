69552fa5557eb4f9bf643ee257c8fac21ff11b44
Krushna-089 <153350440+Krushna-089@users.noreply.github.com>
2026-04-23 11:49:06 +0530

    Create json_db.py

---
+++ json_db.py
@@ -0,0 +1,14 @@
+import json
+import os
+
+DATA_PATH = "data"
+
+def read_json(file):
+    path = os.path.join(DATA_PATH, file)
+    with open(path, "r") as f:
+        return json.load(f)
+
+def write_json(file, data):
+    path = os.path.join(DATA_PATH, file)
+    with open(path, "w") as f:
+        json.dump(data, f, indent=4)
